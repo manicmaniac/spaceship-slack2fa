6f69122701c025cfab38d09bfd37ad80c0bc798d
Ryosuke Ito <rito.0305@gmail.com>
2023-01-13 15:21:55 +0900

    Add documentation comment to module

--- lib/spaceship/slack2fa.rb
+++ lib/spaceship/slack2fa.rb
@@ -8,6 +8,54 @@ require 'spaceship'
 require_relative 'slack2fa/version'
 
 module Spaceship
+  # A module for applying a monkey patch to {https://rubydoc.info/gems/fastlane/Spaceship/Client Spaceship::Client} in the specific scope.
+  #
+  # @example Simple Fastfile
+  #   require "spaceship/slack2fa"
+  #
+  #   ENV["FASTLANE_USER"] = "user@example.com"
+  #   ENV["FASTLANE_PASSWORD"] = "password"
+  #   ENV["SPACESHIP_2FA_SMS_DEFAULT_PHONE_NUMBER"] = "+81 80-XXXX-XXXX"
+  #
+  #   lane :login do
+  #     Spaceship::Slack2fa.enable(
+  #       slack_api_token: "xoxb-0000000000-0000000000000-XXXXXXXXXXXXXXXXXXXXXXXX",
+  #       channel_id: "CXXXXXXXX",
+  #       user_id: "UXXXXXXXX",
+  #       referrer: "My app"
+  #     ) do
+  #       Spaceship::TunesClient.login
+  #     end
+  #   end
+  #
+  # @example Practical Fastfile
+  #   require "spaceship/slack2fa"
+  #
+  #   ENV["FASTLANE_USER"] = "user@example.com"
+  #   ENV["FASTLANE_PASSWORD"] = "password"
+  #   ENV["SPACESHIP_2FA_SMS_DEFAULT_PHONE_NUMBER"] = "+81 80-XXXX-XXXX"
+  #
+  #   lane :release do
+  #     enable_slack2fa do
+  #       deliver
+  #     end
+  #   end
+  #
+  #   lane :beta do
+  #     enable_slack2fa do
+  #       pilot
+  #     end
+  #   end
+  #
+  #   def enable_slack2fa(&block)
+  #     Spaceship::Slack2fa.enable(
+  #       slack_api_token: "xoxb-0000000000-0000000000000-XXXXXXXXXXXXXXXXXXXXXXXX",
+  #       channel_id: "CXXXXXXXX",
+  #       user_id: "UXXXXXXXX",
+  #       referrer: "My app",
+  #       &block
+  #     )
+  #   end
   module Slack2fa
     # Applies monkey patch to {https://rubydoc.info/gems/fastlane/Spaceship/Client Spaceship::Client} so that it retrieves 6-digit code from Slack.
     #
@@ -15,53 +63,6 @@ module Spaceship
     #
     # @param options [Hash] All options are passed to {MonkeyPatch#initialize}.
     # @yield A block where the monkey patch is enabled.
-    #
-    # @example Simple Fastfile
-    #   require "spaceship/slack2fa"
-    #
-    #   ENV["FASTLANE_USER"] = "user@example.com"
-    #   ENV["FASTLANE_PASSWORD"] = "password"
-    #   ENV["SPACESHIP_2FA_SMS_DEFAULT_PHONE_NUMBER"] = "+81 80-XXXX-XXXX"
-    #
-    #   lane :login do
-    #     Spaceship::Slack2fa.enable(
-    #       slack_api_token: "xoxb-0000000000-0000000000000-XXXXXXXXXXXXXXXXXXXXXXXX",
-    #       channel_id: "CXXXXXXXX",
-    #       user_id: "UXXXXXXXX",
-    #       referrer: "My app"
-    #     ) do
-    #       Spaceship::TunesClient.login
-    #     end
-    #   end
-    #
-    # @example Practical Fastfile
-    #   require "spaceship/slack2fa"
-    #
-    #   ENV["FASTLANE_USER"] = "user@example.com"
-    #   ENV["FASTLANE_PASSWORD"] = "password"
-    #   ENV["SPACESHIP_2FA_SMS_DEFAULT_PHONE_NUMBER"] = "+81 80-XXXX-XXXX"
-    #
-    #   lane :release do
-    #     enable_slack2fa do
-    #       deliver
-    #     end
-    #   end
-    #
-    #   lane :beta do
-    #     enable_slack2fa do
-    #       pilot
-    #     end
-    #   end
-    #
-    #   def enable_slack2fa(&block)
-    #     Spaceship::Slack2fa.enable(
-    #       slack_api_token: "xoxb-0000000000-0000000000000-XXXXXXXXXXXXXXXXXXXXXXXX",
-    #       channel_id: "CXXXXXXXX",
-    #       user_id: "UXXXXXXXX",
-    #       referrer: "My app",
-    #       &block
-    #     )
-    #   end
     def self.enable(**options)
       patch = MonkeyPatch.new(**options)
       patch.enable
