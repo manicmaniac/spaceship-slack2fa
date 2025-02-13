2876f964e93afa1b7839212dc0bdbc48cce1ac32
Ryosuke Ito <rito.0305@gmail.com>
2025-02-14 02:47:21 +0900

    Run rubocop -a

--- spec/spaceship/slack2fa/verification_code_not_found_spec.rb
+++ spec/spaceship/slack2fa/verification_code_not_found_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe Spaceship::Slack2fa::VerificationCodeNotFound do
   it 'has a message' do
-    expect(described_class.new.message).to match /2FA code was sent but not found in Slack/
+    expect(described_class.new.message).to match(/2FA code was sent but not found in Slack/)
   end
 end
